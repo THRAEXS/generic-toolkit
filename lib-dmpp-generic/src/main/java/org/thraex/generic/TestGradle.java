073319fc14f6ef5c335ac63520e5fa4c19415115
鬼王 <THRAEX@aliyun.com>
2022-04-11 18:32:34 +0800

    save(chore): build.gradle

--- lib-dmpp-generic/src/main/java/org/thraex/generic/TestGradle.java
+++ lib-dmpp-generic/src/main/java/org/thraex/generic/TestGradle.java
@@ -6,8 +6,11 @@ public class TestGradle {
         System.out.println("Test1...");
     }
 
-    public void test2() {
+    /*public ResponseResult<String> test2() {
+        ResponseResult<String> test = ResponseResult.ok("Test");
+        System.out.println(test);
         System.out.println("Test2...");
-    }
+        return test;
+    }*/
 
 }
