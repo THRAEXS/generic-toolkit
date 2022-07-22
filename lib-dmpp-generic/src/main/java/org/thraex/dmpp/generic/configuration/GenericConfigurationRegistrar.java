414932e30fb3ed6a572c9f25a27b397a9d3b3871
鬼王 <THRAEX@aliyun.com>
2022-07-22 20:42:43 +0800

    opt(generic): import configuration

--- lib-dmpp-generic/src/main/java/org/thraex/dmpp/generic/configuration/GenericConfigurationRegistrar.java
+++ lib-dmpp-generic/src/main/java/org/thraex/dmpp/generic/configuration/GenericConfigurationRegistrar.java
@@ -5,16 +5,17 @@ import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.util.ClassUtils;
 import org.thraex.dmpp.generic.doc.Documentation;
 import org.thraex.toolkit.configuration.AuditorAwareConfiguration;
 import org.thraex.toolkit.configuration.HandlerRoutingConfiguration;
 import org.thraex.toolkit.configuration.TemporalFormatConfiguration;
 import org.thraex.toolkit.configuration.WrapperCodecsConfiguration;
 import org.thraex.toolkit.exception.HandlerFunctionExceptionHandler;
-import reactor.util.Logger;
-import reactor.util.Loggers;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -25,8 +26,6 @@ import java.util.stream.Stream;
  * @date 2022/04/15 22:16
  */
 public class GenericConfigurationRegistrar implements ImportBeanDefinitionRegistrar {
-
-    private static final Logger logger = Loggers.getLogger(GenericConfigurationRegistrar.class);
 
     @Override
     public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata,
@@ -41,21 +40,17 @@ public class GenericConfigurationRegistrar implements ImportBeanDefinitionRegist
     }
 
     public static List<Class<?>> list() {
-        final List<Class<?>> classes = Stream.of(
+        // TODO: Optimization
+        Supplier<Class<?>> supplier = () -> ClassUtils.isPresent("org.springframework.data.domain.AuditorAware",
+                null) ? AuditorAwareConfiguration.class : null;
+
+        return Stream.of(
                 TemporalFormatConfiguration.class,
                 HandlerRoutingConfiguration.class,
                 WrapperCodecsConfiguration.class,
                 HandlerFunctionExceptionHandler.class,
-                Documentation.class).collect(Collectors.toList());
-
-        try {
-            Class.forName("org.springframework.data.domain.AuditorAware");
-            classes.add(AuditorAwareConfiguration.class);
-        } catch (ClassNotFoundException e) {
-            logger.warn("Class not found: [{}]", e.getMessage());
-        }
-
-        return classes;
+                Documentation.class,
+                supplier.get()).filter(Objects::nonNull).collect(Collectors.toList());
     }
 
 }
